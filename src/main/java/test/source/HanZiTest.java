5161275fd6eafd9ff4016985c91aabc146a00698
yangqf <yangqf@shishike.com>
2016-04-07 17:23:17 +0800

    输出汉字测试

---
+++ src/main/java/test/source/HanZiTest.java
@@ -0,0 +1,18 @@
+package test.source;
+
+import org.junit.*;
+import vm.lib.Log;
+import vm.runtime.VirtualMachine;
+
+/**
+ * @author yangqf
+ * @version 1.0 2016/4/7
+ */
+public class HanZiTest{
+
+    public static void main(String[] args){
+        Log log = new Log();
+        log.println("汉字");
+    }
+
+}
